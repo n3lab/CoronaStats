d4675a25e7777e7fdf0856f52120909f82b3f120
n3lab <n3laboratory@yandex.ru>
2021-02-13 01:28:52 +0300

    Initial Commit

---
+++ CoronaStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoronaStats
+//
+//  Created by n3deep on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
